6261343b9b46f380702debd5d8b147c31e2fdca6
LSanchez08 <LUIS.ALBERTO.SANCHEZ.RIOS@LIVE.COM.MX>
2020-12-18 17:07:38 -0600

    m

--- Heaps/Heap.cs
+++ Heaps/Heap.cs
@@ -4,38 +4,256 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Heaps
 {
-    class Heap<T> : IHeap<T> 
+    class Heap<T> : IHeap<T>
     {
         private int size;
-        private int height;
-        private Node<T> rootNode;
+        private double? height;
+        private List<T> valores = new List<T>();
 
 
-        public int Size { get => size; set => size = value; }
-        public int Height { get => height; set => height = value; }
-        public Node<T> RootNode { get => rootNode; private set => rootNode = value; }
-        
+
+        public int Size { get => valores.Count(); set => size = value; }
+        public double? Height { get => GetHeightOfIndex(Size); set => height = value; }
+
+        public Heap()
+        {
+            if (!typeof(IComparable).IsAssignableFrom(typeof(T)))
+            {
+                throw new Exception("Object of Type <T> must implement the Icomparable Interface");
+            }
+            Size = 0;
+            Height = null;
+        }
+
+        public Heap(List<T> data)
+        {
+            if (!typeof(IComparable).IsAssignableFrom(typeof(T)))
+            {
+                throw new Exception("Object of Type <T> must implement the Icomparable Interface");
+            }
+            Size = data.Count;
+            Height = GetHeightOfIndex(Size - 1);
+            valores = data;
+            Heapify(1);
+
+            //foreach(T value in data)
+            //{
+            //    if (Comparer<T>.Default.Compare(first, value) == 1 ) { first = value; }
+            //}
+            //MinData = first;
+        }
+
 
         public void Insert(T element)
         {
-            throw new NotImplementedException();
+            valores.Add(element);
+            UpHeapBubbling(Size - 1);
         }
 
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            return Size == 0;
         }
 
         public T Min()
         {
-            throw new NotImplementedException();
+            if (IsEmpty()) { throw new Exception("The heap is empty"); }
+            T first = valores[0];
+            int index = 0;
+
+            for (int x = 1; x < Size; x++)
+            {
+                if (Comparer<T>.Default.Compare(first, valores[x]) == 1) 
+                {
+                    first = valores[x];
+                    index = x; 
+                }
+            }
+            
+            return valores[index];
         }
 
         public T RemoveMin()
         {
-            throw new NotImplementedException();
+            if (IsEmpty()) { throw new Exception("The heap is empty"); }
+
+            T delete = Min();
+            valores[0] = valores[Size - 1];
+            valores.RemoveAt(Size - 1);
+            DownHeapBubbling(0);
+
+            return delete;
+
         }
+
+        public void UpHeapBubbling(int index)
+        {
+            int indexActual = index;
+            while (indexActual > 0)
+            {
+                if (Comparer<T>.Default.Compare(valores[getParentIndex(indexActual)], valores[indexActual]) == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    T aux = valores[getParentIndex(indexActual)];
+                    valores[getParentIndex(indexActual)] = valores[indexActual];
+                    valores[indexActual] = aux;
+                }
+                indexActual = getParentIndex(indexActual);
+            }
+        }
+
+        public void DownHeapBubbling(int index)
+        {
+
+            int indexActual = index;
+            while (GetHeightOfIndex(indexActual) < Height)
+            {
+                int minChildIndex;
+                if (getLeftChildIndex(indexActual)<Size && getRightChildIndex(indexActual) > Size-1)
+                {
+                    minChildIndex = getLeftChildIndex(indexActual);
+                }
+                else if (Comparer<T>.Default.Compare(valores[getLeftChildIndex(indexActual)], valores[getRightChildIndex(indexActual)]) == 1)
+                {
+                    minChildIndex = getRightChildIndex(indexActual);
+                }
+                else
+                {
+                    minChildIndex = getLeftChildIndex(indexActual);
+                }
+
+
+                if (Comparer<T>.Default.Compare(valores[indexActual], valores[minChildIndex]) == 1)
+                {
+                    T aux = valores[indexActual];
+                    valores[indexActual] = valores[minChildIndex];
+                    valores[minChildIndex] = aux;
+                }
+                else
+                {
+                    break;
+                }
+
+                indexActual = minChildIndex;
+            }
+        }
+
+        private void Heapify_rec(int n, int i, int type)
+        {
+            int largest = i;
+            int left = getLeftChildIndex(i);
+            int right = getRightChildIndex(i);
+            if (left < n && Comparer<T>.Default.Compare(valores[i], valores[left]) == type)
+            {
+                largest = left;
+            }
+
+            if (right < n && Comparer<T>.Default.Compare(valores[largest], valores[right]) == type)
+            {
+                largest = right;
+            }
+
+            if (largest != i)
+            {
+                T aux = valores[i];
+                valores[i] = valores[largest];
+                valores[largest] = aux;
+                Heapify_rec(n, largest, type);
+            }
+
+            
+        }
+
+        public void Heapify(int type)
+        {
+
+            for (int x = valores.Count; x > -1; x--)
+            {
+                Heapify_rec(valores.Count, x, type);
+            }
+
+        }
+
+        public void HeapSort(int type)
+        {
+            Heapify(type);
+            int n = valores.Count;
+            
+            for (int x = n - 1; x > 0; x--)
+            {
+                T aux = valores[x];
+                valores[x] = valores[0];
+                valores[0] = aux;
+                Heapify_rec(x, 0, type);
+            }
+        }
+
+        private static void Heapify_rec(int n, int i, int type, List<T> valores)
+        {
+            int largest = i;
+            int left = (2 * i) + 1;
+            int right = (2 * i) + 2;
+            if (left < n && Comparer<T>.Default.Compare(valores[i], valores[left]) == type)
+            {
+                largest = left;
+            }
+
+            if (right < n && Comparer<T>.Default.Compare(valores[largest], valores[right]) == type)
+            {
+                largest = right;
+            }
+
+            if (largest != i)
+            {
+                T aux = valores[i];
+                valores[i] = valores[largest];
+                valores[largest] = aux;
+                Heapify_rec(n, largest, type, valores);
+            }
+
+
+        }
+
+        public static List<T> Heapify(int type, List<T> valores)
+        {
+
+            for (int x = valores.Count; x > -1; x--)
+            {
+                Heapify_rec(valores.Count, x, type, valores);
+            }
+            return valores;
+
+        }
+
+        public static List<T> HeapSort(int type, List<T> valores)
+        {
+            Heapify(type, valores);
+            int n = valores.Count;
+
+            for (int x = n - 1; x > 0; x--)
+            {
+                T aux = valores[x];
+                valores[x] = valores[0];
+                valores[0] = aux;
+                Heapify_rec(x, 0, type, valores);
+            }
+
+            return valores;
+        }
+
+
+        private int getParentIndex(int index) => (index-1)/2;
+        private int getLeftChildIndex(int index) => (2*index)+1;
+        private int getRightChildIndex(int index) => (2*index)+2;
+
+        public double GetHeightOfIndex(int index) => Math.Round(Math.Truncate(Math.Log((index + 1), 2)));
+
+        public override string ToString() => string.Join( ",", valores.ToArray());
     }
 }
--- Heaps/Program.cs
+++ Heaps/Program.cs
@@ -10,6 +10,31 @@ namespace Heaps
     {
         static void Main(string[] args)
         {
+            List<int> num = new List<int> { 9, 7, 3, 5, 1, 3, 10 };
+
+            Heap<int> heap = new Heap<int>(num);
+
+            heap.Heapify(1);
+            // heap.HeapSort(1);
+            Console.WriteLine(heap.ToString());
+            Console.WriteLine(heap.RemoveMin());
+            Console.WriteLine(heap.ToString());
+            //heap.Insert(4);
+            //Console.WriteLine(heap.ToString());
+            Console.WriteLine("End");
+
+            List<int> num2 = new List<int> { 4, 8, 3, 7, 4, 1 };
+
+            
+
+            Console.WriteLine(string.Join(",", Heap<int>.Heapify(1, num2).ToArray()));
+
+            Console.WriteLine("_____HeapSort_____");
+            Console.WriteLine(string.Join(",", Heap<int>.HeapSort(-1, num2).ToArray()));
+            Console.ReadLine();
         }
+    }
+    public class Auto{
+
     }
 }
